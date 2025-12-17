b273bcb36b674ccf19a9d162f26d332ee3945814
Dip <dafanrushyda@gmail.com>
2025-12-17 15:07:13 +0700

    Update Constants.kt

--- app/src/main/java/com/example/nutriscan/app/utils/Constants.kt
+++ app/src/main/java/com/example/nutriscan/app/utils/Constants.kt
@@ -1,15 +1,14 @@
 package com.nutriscan.app.utils
 
 object Constants {
-    const val SUPABASE_URL = "https://pxlkkqnxenajnmqcypee.supabase.co"
-    const val SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB4bGtrcW54ZW5ham5tcWN5cGVlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQwNDMzNDIsImV4cCI6MjA3OTYxOTM0Mn0.MSuAOALQiYA90Qvq_5fEZeKXmKDnDEQbBpsFHfzJFy0"
+    const val SUPABASE_URL = ""
+    const val SUPABASE_KEY = ""
 
+    const val OCR_SPACE_API_KEY = ""
+    const val OCR_SPACE_URL = ""
 
-    const val OCR_SPACE_API_KEY = "K85339371588957"
-    const val OCR_SPACE_URL = "https://api.ocr.space/parse/image"
+    const val GEMINI_API_KEY = ""
+    const val GEMINI_API_URL = ""
 
-    const val GEMINI_API_KEY = "AIzaSyDXH92TZoLZD3_Ge87CcFy8k68X-Ez6uPg"
-    const val GEMINI_API_URL = "https://generativelanguage.googleapis.com/v1beta/models"
-
-    const val STORAGE_BUCKET = "product-scans"
+    const val STORAGE_BUCKET = ""
 }
